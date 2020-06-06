a83d8d01cd68e2de43fcb648f2eeb6823ccadf33
kalacey412 <61294978+kalacey412@users.noreply.github.com>
2020-06-06 23:34:33 +0300

    Lab 2.3 completed

---
+++ Lab2/Lab2.3/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace lab2._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eneter your string:\t");
+            string InputString = Console.ReadLine();
+            string[] Words = InputString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+            int count = 0;
+            foreach (string Word in Words)
+            {
+                if (Word[Word.Length - 1] == ',' || Word[Word.Length - 1] == '.')
+                {
+                    char pad = Word[Word.Length - 1];
+                    Words[count] = Word.PadLeft(Word.Length + 1, pad);
+                    count++;
+                }
+                else
+                {
+                    count++;
+                }
+            }
+            string FinalString = String.Join(" ", Words);
+            Console.WriteLine(FinalString);
+            Console.ReadLine();
+        }
+    }
+}
