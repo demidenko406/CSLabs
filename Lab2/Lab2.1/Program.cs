6a99a834dc35a4007cfec86ba98fb6a58d87a666
kalacey412 <61294978+kalacey412@users.noreply.github.com>
2020-06-06 23:32:16 +0300

    Lab2.1 Completed

---
+++ Lab2/Lab2.1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FullString;
+            Console.WriteLine("Enter string:\t");
+            FullString = Console.ReadLine();
+            string FinalString = string.Join(" ", FullString.Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries));
+            string[] Words = FinalString.Split(' ');
+
+            for (int i = Words.Length - 1; i >= 0; i--)
+            {
+                Console.Write(Words[i] + " ");
+            }
+
+        }
+    }
+}
