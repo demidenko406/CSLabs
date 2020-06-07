ea486fc93a68478df8ebeca4a3a526ecc258ee31
kalacey412 <61294978+kalacey412@users.noreply.github.com>
2020-06-07 16:53:54 +0300

    Complete

---
+++ lab4.2/MyLib.cpp
@@ -0,0 +1,29 @@
+﻿#include "pch.h"
+#include "MyHeader.h"
+
+extern "C" __declspec(dllexport) int __stdcall Add(int a, int b)
+{
+	return a + b;
+}
+
+extern "C" __declspec(dllexport) int __stdcall Sub(int a, int b)
+{
+	return a - b;
+}
+
+extern "C" __declspec(dllexport) int __stdcall Mult(int a, int b)
+{
+	return a * b;
+}
+
+extern "C" __declspec(dllexport) int __stdcall Div(int a, int b)
+{
+	return a / b;
+}
+
+extern "C" __declspec(dllexport) int __stdcall Power(int a, int b)
+{
+	for (int i = 1; i < b; ++i)
+		a *= a;
+	return a;
+}
---
+++ lab4.2/MyLib.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+extern "C" __declspec(dllexport) int __stdcall Add(int a, int b);
+
+extern "C" __declspec(dllexport) int __stdcall Sub(int a, int b);
+
+extern "C" __declspec(dllexport) int __stdcall Mult(int a, int b);
+
+extern "C" __declspec(dllexport) int __stdcall Div(int a, int b);
+
+extern "C" __declspec(dllexport) int __stdcall Power(int a, int b);
